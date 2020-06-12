df34ace1fec296c0a25ceb5f57bd5a11b1cfce2e
David Kim <kimbyi@bc.edu>
2020-06-12 12:59:39 -0400

    console instead of print

--- django_ml/settings.py
+++ django_ml/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*e-vifgtz8^38rhj*x%&-k_&vs+v*24g1r_+#6atve@scpwylo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["kingdawei.pythonanywhere.com"]
+ALLOWED_HOSTS = ["kingdawei.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
